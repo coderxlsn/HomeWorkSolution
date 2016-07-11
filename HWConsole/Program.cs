ef4736612f09e0eaebda3b90752df62615db3e76
artur <coderxlsn@hotmail.com>
2016-07-11 22:43:09 +0300

    готово

--- HWConsole/Program.cs
+++ HWConsole/Program.cs
@@ -10,6 +10,8 @@ namespace HWConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Artur");
+            Console.ReadKey();
         }
     }
 }
--- HWConsoleBackground/Program.cs
+++ HWConsoleBackground/Program.cs
@@ -10,6 +10,10 @@ namespace HWConsoleBackground
     {
         static void Main(string[] args)
         {
+            DateTime dt =  DateTime.Now;
+            ;
+            Console.WriteLine("Сейчас {0}, {1}",dt.ToLongDateString(), dt.ToLongTimeString());
+            Console.ReadKey();
         }
     }
 }
